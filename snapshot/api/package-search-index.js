4c39d3beb1b76e081d78e8c70c1bb302bd4a5e7d
puneetbehl <behlp@unityfoundation.io>
2024-12-07 14:17:33 +0000

    Deploying to gh-pages - 14:17:32

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"grails.plugins.quartz"},{"l":"grails.plugins.quartz.listeners"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"grails.plugins.quartz"},{"l":"grails.plugins.quartz.listeners"}];updateSearchResults();
